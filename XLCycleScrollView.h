0e52ea03501dafc0127fd5661f77e1b85e81d324
ChenFromNB <chendi58@gmail.com>
2014-05-16 17:23:03 +0800

    add new

---
+++ XLCycleScrollView.h
@@ -0,0 +1,56 @@
+//
+//  XLCycleScrollView.h
+//  CycleScrollViewDemo
+//
+//  Created by xie liang on 9/14/12.
+//  Copyright (c) 2012 xie liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol XLCycleScrollViewDelegate;
+@protocol XLCycleScrollViewDatasource;
+
+@interface XLCycleScrollView : UIView<UIScrollViewDelegate>
+{
+    UIScrollView *_scrollView;
+    UIPageControl *_pageControl;
+    
+    id<XLCycleScrollViewDelegate> _delegate;
+    id<XLCycleScrollViewDatasource> _datasource;
+    
+    NSInteger _totalPages;
+    NSInteger _curPage;
+    
+    NSMutableArray *_curViews;
+}
+
+@property (nonatomic,readonly) UIScrollView *scrollView;
+@property (nonatomic,readonly) UIPageControl *pageControl;
+@property (nonatomic,assign) NSInteger currentPage;
+@property (nonatomic,assign,setter = setDataource:) id<XLCycleScrollViewDatasource> datasource;
+@property (nonatomic,assign,setter = setDelegate:) id<XLCycleScrollViewDelegate> delegate;
+
+- (void)reloadData;
+- (void)setViewContent:(UIView *)view atIndex:(NSInteger)index;
+//自动翻页
+/*
+- (void)free;
+*/
+
+@end
+
+@protocol XLCycleScrollViewDelegate <NSObject>
+
+@optional
+- (void)didClickPage:(XLCycleScrollView *)csView atIndex:(NSInteger)index;
+
+@end
+
+@protocol XLCycleScrollViewDatasource <NSObject>
+
+@required
+- (NSInteger)numberOfPages;
+- (UIView *)pageAtIndex:(NSInteger)index;
+
+@end
---
+++ XLCycleScrollView.m
@@ -0,0 +1,212 @@
+//
+//  XLCycleScrollView.m
+//  CycleScrollViewDemo
+//
+//  Created by xie liang on 9/14/12.
+//  Copyright (c) 2012 xie liang. All rights reserved.
+//
+
+#import "XLCycleScrollView.h"
+#import "NSTimer+Addition.h"
+
+@interface XLCycleScrollView()
+
+//@property (nonatomic , strong) NSTimer *animationTimer;
+//@property (nonatomic , assign) NSTimeInterval animationDuration;
+
+@end
+
+@implementation XLCycleScrollView
+
+@synthesize scrollView = _scrollView;
+@synthesize pageControl = _pageControl;
+@synthesize currentPage = _curPage;
+@synthesize datasource = _datasource;
+@synthesize delegate = _delegate;
+
+- (void)dealloc
+{
+    //[_animationTimer release];
+    [_scrollView release];
+    [_pageControl release];
+    [_curViews release];
+    [super dealloc];
+}
+
+//自动翻页
+/*
+//减少父控件的引用计数
+- (void)free
+{
+    [_animationTimer invalidate];
+}
+ */
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        _scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+        _scrollView.delegate = self;
+        _scrollView.contentSize = CGSizeMake(self.frame.size.width * 3, self.frame.size.height);
+        _scrollView.showsHorizontalScrollIndicator = NO;
+        _scrollView.contentOffset = CGPointMake(self.frame.size.width, 0);
+        _scrollView.pagingEnabled = YES;
+        [self addSubview:_scrollView];
+        
+        CGRect rect = CGRectMake(80, self.bounds.size.height - 20, 160, 20);
+        _pageControl = [[UIPageControl alloc] initWithFrame:rect];
+        _pageControl.userInteractionEnabled = NO;
+        [self addSubview:_pageControl];
+        
+        _curPage = 0;
+        //自动翻页
+        /*
+        self.animationDuration = 5.0f;
+        if (_animationDuration > 0.0) {
+            self.animationTimer = [NSTimer scheduledTimerWithTimeInterval:self.animationDuration
+                                                                   target:self
+                                                                 selector:@selector(animationTimerDidFired:)
+                                                                 userInfo:nil
+                                                                  repeats:YES];
+            [self.animationTimer pauseTimer];
+        }
+         */
+    }
+    return self;
+}
+
+- (void)setDataource:(id<XLCycleScrollViewDatasource>)datasource
+{
+    _datasource = datasource;
+    [self reloadData];
+}
+
+- (void)reloadData
+{
+    _totalPages = [_datasource numberOfPages];
+    if (_totalPages == 0) {
+        return;
+    }
+    _pageControl.numberOfPages = _totalPages;
+    //[self.animationTimer resumeTimerAfterTimeInterval:self.animationDuration];
+    [self loadData];
+}
+
+- (void)loadData
+{
+    
+    _pageControl.currentPage = _curPage;
+    
+    //从scrollView上移除所有的subview
+    NSArray *subViews = [_scrollView subviews];
+    if([subViews count] != 0) {
+        [subViews makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    }
+    
+    [self getDisplayImagesWithCurpage:_curPage];
+    
+    for (int i = 0; i < 3; i++) {
+        UIView *v = [_curViews objectAtIndex:i];
+        v.userInteractionEnabled = YES;
+        UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                                    action:@selector(handleTap:)];
+        [v addGestureRecognizer:singleTap];
+        [singleTap release];
+        v.frame = CGRectOffset(v.frame, v.frame.size.width * i, 0);
+        [_scrollView addSubview:v];
+    }
+    
+    [_scrollView setContentOffset:CGPointMake(_scrollView.frame.size.width, 0)];
+}
+
+- (void)getDisplayImagesWithCurpage:(int)page {
+    
+    int pre = [self validPageValue:_curPage-1];
+    int last = [self validPageValue:_curPage+1];
+    
+    if (!_curViews) {
+        _curViews = [[NSMutableArray alloc] init];
+    }
+    
+    [_curViews removeAllObjects];
+    [_curViews addObject:[_datasource pageAtIndex:pre]];
+    [_curViews addObject:[_datasource pageAtIndex:page]];
+    [_curViews addObject:[_datasource pageAtIndex:last]];
+}
+
+- (int)validPageValue:(NSInteger)value {
+    
+    if(value == -1) value = _totalPages - 1;
+    if(value == _totalPages) value = 0;
+    
+    return value;
+}
+
+- (void)handleTap:(UITapGestureRecognizer *)tap {
+    
+    if ([_delegate respondsToSelector:@selector(didClickPage:atIndex:)]) {
+        [_delegate didClickPage:self atIndex:_curPage];
+    }
+    
+}
+
+- (void)setViewContent:(UIView *)view atIndex:(NSInteger)index
+{
+    if (index == _curPage) {
+        [_curViews replaceObjectAtIndex:1 withObject:view];
+        for (int i = 0; i < 3; i++) {
+            UIView *v = [_curViews objectAtIndex:i];
+            v.userInteractionEnabled = YES;
+            UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                                        action:@selector(handleTap:)];
+            [v addGestureRecognizer:singleTap];
+            [singleTap release];
+            v.frame = CGRectOffset(v.frame, v.frame.size.width * i, 0);
+            [_scrollView addSubview:v];
+        }
+    }
+}
+
+#pragma mark - UIScrollViewDelegate
+- (void)scrollViewDidScroll:(UIScrollView *)aScrollView {
+    int x = aScrollView.contentOffset.x;
+    
+    //往下翻一张
+    if(x >= (2*self.frame.size.width)) {
+        _curPage = [self validPageValue:_curPage+1];
+        [self loadData];
+    }
+    
+    //往上翻
+    if(x <= 0) {
+        _curPage = [self validPageValue:_curPage-1];
+        [self loadData];
+    }
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)aScrollView {
+    
+    [_scrollView setContentOffset:CGPointMake(_scrollView.frame.size.width, 0) animated:YES];
+}
+
+/*
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
+{
+    [self.animationTimer pauseTimer];
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+{
+    [self.animationTimer resumeTimerAfterTimeInterval:self.animationDuration];
+}
+ */
+
+- (void)animationTimerDidFired:(NSTimer *)timer
+{
+    CGPoint newOffset = CGPointMake(self.scrollView.contentOffset.x + CGRectGetWidth(self.scrollView.frame), self.scrollView.contentOffset.y);
+    [self.scrollView setContentOffset:newOffset animated:YES];
+}
+
+@end
